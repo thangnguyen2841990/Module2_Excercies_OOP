93924b832a15da183da09892649fdf3f28fc70b4
thangnguyen2841990 <96616281+thangnguyen2841990@users.noreply.github.com>
2022-01-14 14:13:03 +0700

    done

---
+++ src/com/codegym/Familly.java
@@ -0,0 +1,55 @@
+package com.codegym;
+
+import java.util.Arrays;
+
+public class Familly {
+    private int famillyMembers;
+    private int address;
+    private Person[] members = new Person[famillyMembers];
+
+
+    public Familly() {
+
+    }
+
+    public Familly(int famillyMembers, int address, Person[] members) {
+        this.famillyMembers = famillyMembers;
+        this.address = address;
+        this.members = members;
+    }
+
+    public int getFamillyMembers() {
+        return famillyMembers;
+    }
+
+    public void setFamillyMembers(int famillyMembers) {
+        this.famillyMembers = famillyMembers;
+    }
+
+    public int getAddress() {
+        return address;
+    }
+
+    public void setAddress(int address) {
+        this.address = address;
+    }
+
+    public Person[] getMembers() {
+        return members;
+    }
+
+    public void setMembers(Person[] members) {
+        this.members = members;
+    }
+
+    public void displayAllMember() {
+        for (int i = 0; i < members.length; i++) {
+            System.out.println(members[i]);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Tổng số thành viên trong nhà: " + this.famillyMembers + ", số nhà: " + this.address + "\nCác thành viên trong gia đình: ";
+    }
+}
---
+++ src/com/codegym/FamillyMangement.java
@@ -0,0 +1,65 @@
+package com.codegym;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class FamillyMangement {
+    Familly[] famillies = new Familly[0];
+
+
+    public FamillyMangement() {
+
+    }
+
+    public FamillyMangement(Familly[] famillies) {
+        this.famillies = famillies;
+    }
+
+    public Familly[] getFamillies() {
+        return famillies;
+    }
+
+    public void setFamillies(Familly[] famillies) {
+        this.famillies = famillies;
+    }
+
+    public void displayAllFamillies() {
+        for (int i = 0; i < famillies.length; i++) {
+            System.out.println((i + 1) + ". " + famillies[i]);
+            famillies[i].displayAllMember();
+        }
+    }
+
+    public void addNewFamilly(int index, Familly newFamilly) {
+        Familly[] newFamillies = new Familly[famillies.length + 1];
+        for (int i = 0; i < newFamillies.length; i++) {
+            if (i < index) {
+                newFamillies[i] = famillies[i];
+            } else if (i == index) {
+                newFamillies[i] = newFamilly;
+
+            } else {
+                newFamillies[i] = famillies[i - 1];
+            }
+        }
+        this.famillies = newFamillies;
+    }
+
+    public void removeFamilly(int index) {
+        Familly[] newFamillies = new Familly[famillies.length - 1];
+        for (int i = 0; i < newFamillies.length; i++) {
+            if (i < index) {
+                newFamillies[i] = famillies[i];
+            } else {
+                newFamillies[i] = famillies[i + 1];
+            }
+        }
+        this.famillies = newFamillies;
+    }
+
+    public void updateFamilly(int index, Familly newFamilly) {
+        famillies[index] = newFamilly;
+    }
+
+
+}
